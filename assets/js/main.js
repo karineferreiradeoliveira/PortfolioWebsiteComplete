e19499fcff48e799716f7a4ac92cedf2a17269f5
karine ferroli <karinepro@outlook.com>
2021-11-05 20:44:20 -0300

    primeiro Commit

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+
+    }
+    
+
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction (){
+   //Active Link
+    navLink.forEach(n=> n.classList.remove('active'))
+    this.classList.add('active')  
+    
+    //Remove Menu Mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+
+}  
+navLink.forEach(n => n.addEventListener('click', linkAction ))
+  /*Active link*/
+  
+  /*Remove menu mobile*/
+  
+/*===== SCROLL REVEAL ANIMATION =====*/
+
+/*SCROLL HOME*/
+
+/*SCROLL ABOUT*/
+
+/*SCROLL SKILLS*/
+
+/*SCROLL WORK*/
+
+/*SCROLL CONTACT*/
+
+
+
+
+
+
+
